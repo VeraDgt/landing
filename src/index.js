c511479f9b72bafbafdee0ec5e16d8d6bedacd18
VeraDgt <veradgt@gmail.com>
2023-08-21 10:57:48 +0300

    fix/slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles/index.css'; 
+
+
+const switcherList = Array.from(document.querySelectorAll('.switcher'));
+
+  switcherList.forEach((switcher) => {
+    switcher.addEventListener("click", function() {
+      const switcherElements = Array.from(switcher.querySelectorAll('.switcher__element'));
+      switcherElements.forEach((element) => {
+        element.classList.toggle('switcher__element_active');
+      });
+      const movedElement = switcher.previousElementSibling;
+      movedElement.classList.contains('articles') ? movedElement.classList.toggle('articles_move_left') : movedElement.classList.toggle('pictures_move_left');
+    })
+  });
+
+
